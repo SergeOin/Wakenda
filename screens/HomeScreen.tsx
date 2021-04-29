a23aca48f112cfb54c6c60ce8c5a15c294c66054
SergeOin <p.logre@hotmail.fr>
2021-04-29 19:03:21 +0200

    Commit BuilderX

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import {
-  ActivityIndicator,
+  ActivityIndicator, Image,
   Platform,
+  SafeAreaView,
+  StatusBar,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-
-import {UserContext} from '../UserContext';
+import CupertinoHeaderWithLargeTitle from '../components/CupertinoHeaderWithLargeTitle';
 
 const Stack = createStackNavigator();
 
 const HomeComponent = () => {
-  const userContext = React.useContext(UserContext);
-
   return (
     <View style={styles.container}>
-      <ActivityIndicator
-        color={Platform.OS === 'android' ? '#276b80' : undefined}
-        animating={userContext.userLoading}
-        size="large"
+      <CupertinoHeaderWithLargeTitle
+        style={styles.cupertinoHeaderWithLargeTitle}
       />
-      {userContext.userLoading ? null : (
-        <Text>Hello {userContext.userFirstName}!</Text>
-      )}
+      <View style={styles.rectStack}>
+        <View style={styles.rect} />
+        <Image
+          source={require('../assets/images/dash_score.png')}
+          resizeMode="contain"
+          style={styles.image}
+        />
+      </View>
+      <Image
+        source={require('../assets/images/Dash_axe.png')}
+        resizeMode="contain"
+        style={styles.image2}
+      />
     </View>
   );
 };
@@ -48,7 +55,40 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'rgba(232,235,244,1)',
+  },
+  cupertinoHeaderWithLargeTitle: {
+    height: 96,
+    width: 375,
+    marginTop: 51,
+    marginLeft: 15,
+  },
+  rect: {
+    top: 0,
+    left: 18,
+    width: 248,
+    height: 292,
+    position: 'absolute',
+    backgroundColor: 'rgba(255,255,255,1)',
+    borderRadius: 21,
+  },
+  image: {
+    top: 7,
+    left: 0,
+    width: 279,
+    height: 279,
+    position: 'absolute',
+  },
+  rectStack: {
+    width: 279,
+    height: 292,
+    marginTop: 8,
+    marginLeft: 48,
+  },
+  image2: {
+    width: 232,
+    height: 263,
+    marginTop: 27,
+    marginLeft: 74,
   },
 });
